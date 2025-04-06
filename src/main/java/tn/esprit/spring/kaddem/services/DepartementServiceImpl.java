c2368f0c716231cda5af7c768204f2d76a76d108
nousseibakaabi <nousseiba.kaabi@esprit.tn>
2025-04-06 16:25:23 +0100

    Update DepartementServiceImpl.java

--- src/main/java/tn/esprit/spring/kaddem/services/DepartementServiceImpl.java
+++ src/main/java/tn/esprit/spring/kaddem/services/DepartementServiceImpl.java
@@ -5,10 +5,9 @@ import org.springframework.stereotype.Service;
 
 import lombok.extern.slf4j.Slf4j;
 import tn.esprit.spring.kaddem.entities.Departement;
-import tn.esprit.spring.kaddem.entities.Equipe;
-import tn.esprit.spring.kaddem.repositories.ContratRepository;
 import tn.esprit.spring.kaddem.repositories.DepartementRepository;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
 @Slf4j
@@ -29,11 +28,14 @@ public class DepartementServiceImpl implements IDepartementService{
 		return departementRepository.save(d);
 	}
 
-	public  Departement retrieveDepartement (Integer idDepart){
-		return departementRepository.findById(idDepart).get();
+	public Departement retrieveDepartement(Integer idDepart) {
+		return departementRepository.findById(idDepart)
+				.orElseThrow(() -> new EntityNotFoundException("Departement not found with id: " + idDepart));
 	}
-	public  void deleteDepartement(Integer idDepartement){
-		Departement d=retrieveDepartement(idDepartement);
+
+	public void deleteDepartement(Integer idDepartement) {
+		Departement d = departementRepository.findById(idDepartement)
+				.orElseThrow(() -> new EntityNotFoundException("Departement not found with id: " + idDepartement));
 		departementRepository.delete(d);
 	}
 
