7820519530d317ebc7c80243f887ad26fb8b2609
Sarra-Sahli <sarah1sahlii1@gmail.com>
2025-04-13 12:43:12 +0200

    promtheus config

---
+++ src/main/java/tn/esprit/spring/kaddem/config/ActuatorConfig.java
@@ -0,0 +1,17 @@
+package tn.esprit.spring.kaddem.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ActuatorConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/actuator/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST")
+                .allowedHeaders("*");
+    }
+} 
